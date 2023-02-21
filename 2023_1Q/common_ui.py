8a463448f7fcb4b69265bb427d729273601ffcfc
frozenash <43192428+frozen4sh@users.noreply.github.com>
2023-02-21 15:05:22 +0900

    202302210305

--- 2023_1Q/common_ui.py
+++ 2023_1Q/common_ui.py
@@ -16,8 +16,8 @@ titleA='sequence_run_script_UI_v20210503'
 class sequence_run_script_UI(mayaMixin.MayaQWidgetBaseMixin,QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(sequence_run_script_UI, self).__init__(parent)
-        if pm.window(u'sequence_run_script_UI_01', q=True, ex=True):
-            pm.deleteUI(u'sequence_run_script_UI_01', window=True)
+        if pm.window('sequence_run_script_UI_01', q=True, ex=True):
+            pm.deleteUI('sequence_run_script_UI_01', window=True)
         self.setObjectName('sequence_run_script_UI_01')
         self.setWindowTitle(titleA)
         self.setGeometry(500, 200, 350, 750)
