2f95668169c404b071e9cc859e1833e748fdcee9
frozenash <hyh1411@hotmail.com>
2023-03-16 11:54:52 +0900

    20230316

--- 2023_1Q/MayaScriptTest/selectMirrorContollers.py
+++ 2023_1Q/MayaScriptTest/selectMirrorContollers.py
@@ -1,46 +1,51 @@
-import maya.cmds as cmds
+# import maya.cmds as cmds
 
 
-# # Define a list of controller names
+# # show current selected controllers
+# selected_objects = []
+# right_controllers = []
+# left_controllers = []
+
+# selected_objects.clear()
+# right_controllers.clear()
+# left_controllers.clear()
 # selected_objects = cmds.ls(selection=True)
 # print(selected_objects)
 
 
-# # Use a list comprehension to create a new list that contains only the right-side controller names
-# right_controllers = [name.replace("_L_", "_R_") for name in selected_objects]
+# # select mirrored right controllers
+# for obj in selected_objects:
+#     if "_L_" in obj:
+#         print("left")
+#         right_controllers = [name.replace("_L_", "_R_") for name in selected_objects]
+        
+        
+#     if "_R_" in obj:
+#         print("right")
+#         left_controllers = [name.replace("_R_", "_L_") for name in selected_objects]
+        
 
-# # Print the list of right-side controller names
-# print(right_controllers)
-# cmds.select(right_controllers)
+# cmds.select(selected_objects, right_controllers, left_controllers)
+
+# import maya.cmds as cmds
+
+# selected_objects = cmds.ls(selection=True)
+# print(selected_objects)
+
+# right_controllers = [name.replace("_L_", "_R_") for name in selected_objects if "_L_" in name]
+# left_controllers = [name.replace("_R_", "_L_") for name in selected_objects if "_R_" in name]
+
+# cmds.select(selected_objects + right_controllers + left_controllers)
 
 
+import maya.cmds as cmds
 
-
-# Define a list of controller names
-
-
-
+# show current selected controllers
 selected_objects = cmds.ls(selection=True)
 print(selected_objects)
 
-for obj in selected_objects:
-    if "_L_" in obj:
-        print("왼쪽")
-        right_controllers = [name.replace("_L_", "_R_") for name in selected_objects]
-        cmds.select(selected_objects, right_controllers)
+# select mirrored right controllers
+right_controllers = [name.replace("_L_", "_R_") for name in selected_objects if "_L_" in name]
+left_controllers = [name.replace("_R_", "_L_") for name in selected_objects if "_R_" in name]
 
-
-for obj in selected_objects:
-    if "_R_" in obj:
-        print("오른쪽")
-        left_controllers = [name.replace("_R_", "_L_") for name in selected_objects]
-        cmds.select(selected_objects, left_controllers)
-
-
-# # Use a list comprehension to create a new list that contains only the right-side controller names
-# right_controllers = [name.replace("_L_", "_R_") for name in selected_objects]
-
-# # Print the list of right-side controller names
-# print(right_controllers)
-# cmds.select(right_controllers)
-
+cmds.select(selected_objects, right_controllers, left_controllers)
