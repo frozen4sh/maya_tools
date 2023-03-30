47037972c6c9b9905de25598cd6bcfc23431f2d3
frozenash <hyh1411@hotmail.com>
2023-03-30 18:29:58 +0900

    20230330

---
+++ test.py
@@ -0,0 +1,178 @@
+setAttr "blendShape7.tooth_d_u" 1;
+setAttr "blendShape7.brow_lower_l" 1;
+setAttr "blendShape7.brow_lower_r" 1;
+setAttr "blendShape7.brow_raise_c" 1;
+setAttr "blendShape7.brow_raise_l" 1;
+setAttr "blendShape7.brow_raise_r" 1;
+setAttr "blendShape7.cheek_puff_l" 1;
+setAttr "blendShape7.cheek_puff_r" 1;
+setAttr "blendShape7.cheek_raise_l" 1;
+setAttr "blendShape7.cheek_raise_r" 1;
+setAttr "blendShape7.cheek_suck_l" 1;
+setAttr "blendShape7.cheek_suck_r" 1;
+setAttr "blendShape7.cheek_UP" 1;
+setAttr "blendShape7.eye_blink1_l" 1;
+setAttr "blendShape7.eye_blink1_r" 1;
+setAttr "blendShape7.eye_blink2_l" 1;
+setAttr "blendShape7.eye_blink2_r" 1;
+setAttr "blendShape7.eye_lidTight_l" 1;
+setAttr "blendShape7.eye_lidTight_r" 1;
+setAttr "blendShape7.eye_shutTight_l" 1;
+setAttr "blendShape7.eye_shutTight_r" 1;
+setAttr "blendShape7.eye_upperLidRaise_l" 1;
+setAttr "blendShape7.eye_upperLidRaise_r" 1;
+setAttr "blendShape7.eye_downLidRaise_l" 1;
+setAttr "blendShape7.eye_downLidRaise_r" 1;
+setAttr "blendShape7.jaw_sideways_l" 1;
+setAttr "blendShape7.jaw_sideways_r" 1;
+setAttr "blendShape7.jaw_thrust_c" 1;
+setAttr "blendShape7.mouth_chew_c" 1;
+setAttr "blendShape7.mouth_chinRaise_d" 1;
+setAttr "blendShape7.mouth_chinRaise_u" 1;
+setAttr "blendShape7.mouth_dimple_l" 1;
+setAttr "blendShape7.mouth_dimple_r" 1;
+setAttr "blendShape7.mouth_funnel_dl" 1;
+setAttr "blendShape7.mouth_funnel_dr" 1;
+setAttr "blendShape7.mouth_funnel_ul" 1;
+setAttr "blendShape7.mouth_funnel_ur" 1;
+setAttr "blendShape7.mouth_lipCornerDepressFix_l" 1;
+setAttr "blendShape7.mouth_lipCornerDepressFix_r" 1;
+setAttr "blendShape7.mouth_lipCornerDepress_l" 1;
+setAttr "blendShape7.mouth_lipCornerDepress_r" 1;
+setAttr "blendShape7.mouth_lipCornerPullOpen_l" 1;
+setAttr "blendShape7.mouth_lipCornerPullOpen_r" 1;
+setAttr "blendShape7.mouth_lipCornerPull_l" 1;
+setAttr "blendShape7.mouth_lipCornerPull_r" 1;
+setAttr "blendShape7.mouth_lipStretchOpen_l" 1;
+setAttr "blendShape7.mouth_lipStretchOpen_r" 1;
+setAttr "blendShape7.mouth_lipStretch_l" 1;
+setAttr "blendShape7.mouth_lipStretch_r" 1;
+setAttr "blendShape7.mouth_lowerLipDepress_l" 1;
+setAttr "blendShape7.mouth_lowerLipDepress_r" 1;
+setAttr "blendShape7.mouth_lowerLipProtrude_c" 1;
+setAttr "blendShape7.mouth_oh_c" 1;
+setAttr "blendShape7.mouth_oo_c" 1;
+setAttr "blendShape7.mouth_pressFix_c" 1;
+setAttr "blendShape7.mouth_press_l" 1;
+setAttr "blendShape7.mouth_press_r" 1;
+setAttr "blendShape7.mouth_pucker_l" 1;
+setAttr "blendShape7.mouth_pucker_r" 1;
+setAttr "blendShape7.mouth_screamFix_c" 1;
+setAttr "blendShape7.mouth_sideways_l" 1;
+setAttr "blendShape7.mouth_sideways_r" 1;
+setAttr "blendShape7.mouth_stretch_c" 1;
+setAttr "blendShape7.mouth_suck_dl" 1;
+setAttr "blendShape7.mouth_suck_dr" 1;
+setAttr "blendShape7.mouth_suck_ul" 1;
+setAttr "blendShape7.mouth_suck_ur" 1;
+setAttr "blendShape7.mouth_upperLipRaise_l" 1;
+setAttr "blendShape7.mouth_upperLipRaise_r" 1;
+setAttr "blendShape7.nose_wrinkle_l" 1;
+setAttr "blendShape7.nose_wrinkle_r" 1;
+setAttr "blendShape7.tooth_l" 1;
+setAttr "blendShape7.eye_lookDown1_l" 1;
+setAttr "blendShape7.eye_lookDown2_l" 1;
+setAttr "blendShape7.eye_lookLeft_l" 1;
+setAttr "blendShape7.eye_lookRight_l" 1;
+setAttr "blendShape7.eye_lookUp_l" 1;
+setAttr "blendShape7.eye_lookDown1_r" 1;
+setAttr "blendShape7.eye_lookDown2_r" 1;
+setAttr "blendShape7.eye_lookLeft_r" 1;
+setAttr "blendShape7.eye_lookRight_r" 1;
+setAttr "blendShape7.eye_lookUp_r" 1;
+setAttr "blendShape7.tongue_Rot_1X" 1;
+setAttr "blendShape7.tongue_Rot__1X" 1;
+setAttr "blendShape7.tongue_Rot_2X" 1;
+setAttr "blendShape7.tongue_Rot__2X" 1;
+setAttr "blendShape7.tongue_Rot_3X" 1;
+setAttr "blendShape7.tongue_Rot__3X" 1;
+setAttr "blendShape7.tongue_Rot_1Y" 1;
+setAttr "blendShape7.tongue_Rot__1Y" 1;
+setAttr "blendShape7.tongue_Rot_2Y" 1;
+setAttr "blendShape7.tongue_Rot__2Y" 1;
+setAttr "blendShape7.tongue_Rot_3Y" 1;
+setAttr "blendShape7.tongue_Rot__3Y" 1;
+setAttr "blendShape7.tongue_Rot_1Z" 1;
+setAttr "blendShape7.tongue_Rot__1Z" 1;
+setAttr "blendShape7.tongue_Rot_2Z" 1;
+setAttr "blendShape7.tongue_Rot__2Z" 1;
+setAttr "blendShape7.tongue_Rot_3Z" 1;
+setAttr "blendShape7.tongue_Rot__3Z" 1;
+setAttr "blendShape7.tongue_Scale_X" 1;
+setAttr "blendShape7.tongue_Scale__X" 1;
+setAttr "blendShape7.tongue_Scale_Y" 1;
+setAttr "blendShape7.tongue_Scale__Y" 1;
+setAttr "blendShape7.tongue_Scale_Z" 1;
+setAttr "blendShape7.tongue_Scale__Z" 1;
+setAttr "blendShape7.nose_out_l" 1;
+setAttr "blendShape7.nose_out_r" 1;
+setAttr "blendShape7.tongue_u" 1;
+setAttr "blendShape7.tongue_u_u" 1;
+setAttr "blendShape7.brow_raise_d" 1;
+setAttr "blendShape7.mouth_stretch_u" 1;
+setAttr "blendShape7.tongue_u_d" 1;
+setAttr "blendShape7.tooth_d_d" 1;
+setAttr "blendShape7.tongue_d" 1;
+setAttr "blendShape7.tooth_r" 1;
+
+
+
+
+import maya.cmds as cmds
+import sys
+#scriptpath =r'C:\Python27\Lib\site-packages'
+scriptpath =r'C:\Users\vivestudios\AppData\Local\Programs\Python\Python37\Lib\site-packages'
+sys.path.append(scriptpath)
+import maya.cmds as cmds
+import numpy as np
+import os
+import sys
+
+
+fullpath = os.path.join("C:\\Users\\vivestudios\\Downloads\\" , "bs_value_1114_1_01.npy")
+
+print (fullpath)
+
+list = ["brow_lower_l","tongue_Scale__X","tongue_Scale_Y","tongue_Scale__Y","tongue_Scale_Z","tongue_Scale__Z","nose_out_l","nose_out_r","tongue_u","tongue_u_u","brow_raise_d","cheek_suck_r","mouth_stretch_u","tongue_u_d","tooth_d_d","tongue_d","tooth_r","tooth_d_u","cheek_UP","eye_blink1_l","eye_blink1_r","eye_blink2_l","eye_blink2_r","eye_lidTight_l","eye_lidTight_r","eye_shutTight_l","eye_shutTight_r","brow_lower_r","eye_upperLidRaise_l","eye_upperLidRaise_r","eye_downLidRaise_l","eye_downLidRaise_r","jaw_sideways_l","jaw_sideways_r","jaw_thrust_c","mouth_chew_c","mouth_chinRaise_d","mouth_chinRaise_u","brow_raise_c","mouth_dimple_l","mouth_dimple_r","mouth_funnel_dl","mouth_funnel_dr","mouth_funnel_ul","mouth_funnel_ur","mouth_lipCornerDepressFix_l","mouth_lipCornerDepressFix_r","mouth_lipCornerDepress_l","mouth_lipCornerDepress_r","brow_raise_l","mouth_lipCornerPullOpen_l","mouth_lipCornerPullOpen_r","mouth_lipCornerPull_l","mouth_lipCornerPull_r","mouth_lipStretchOpen_l","mouth_lipStretchOpen_r","mouth_lipStretch_l","mouth_lipStretch_r","mouth_lowerLipDepress_l","mouth_lowerLipDepress_r","brow_raise_r","mouth_lowerLipProtrude_c","mouth_oh_c","mouth_oo_c","mouth_pressFix_c","mouth_press_l","mouth_press_r","mouth_pucker_l","mouth_pucker_r","mouth_screamFix_c","mouth_sideways_l","cheek_puff_l","mouth_sideways_r","mouth_stretch_c","mouth_suck_dl","mouth_suck_dr","mouth_suck_ul","mouth_suck_ur","mouth_upperLipRaise_l","mouth_upperLipRaise_r","nose_wrinkle_l","nose_wrinkle_r","cheek_puff_r","tooth_l","eye_lookDown1_l","eye_lookDown2_l","eye_lookLeft_l","eye_lookRight_l","eye_lookUp_l","eye_lookDown1_r","eye_lookDown2_r","eye_lookLeft_r","eye_lookRight_r","cheek_raise_l","eye_lookUp_r","tongue_Rot_1X","tongue_Rot__1X","tongue_Rot_2X","tongue_Rot__2X","tongue_Rot_3X","tongue_Rot__3X","tongue_Rot_1Y","tongue_Rot__1Y","tongue_Rot_2Y","cheek_raise_r","tongue_Rot__2Y","tongue_Rot_3Y","tongue_Rot__3Y","tongue_Rot_1Z","tongue_Rot__1Z","tongue_Rot_2Z","tongue_Rot__2Z","tongue_Rot_3Z","tongue_Rot__3Z","tongue_Scale_X","cheek_suck_l"]
+
+
+frames = np.load(fullpath)
+frames.shape
+len(frames[5604])
+print (frames[5606])
+for i in range(0,5607):
+    cmds.currentTime(i)
+    cmds.setAttr('blendShape7.'+list[0] , frames[i][0])
+    cmds.setKeyframe('blendShape7.'+list[0])
+    cmds.setAttr('blendShape7.'+list[1] , frames[i][1])
+    cmds.setKeyframe('blendShape7.'+list[1])    
+"""    
+for i in range(0,5607):
+    cmds.currentTime(i)
+    cmds.setAttr('blendShape7.'+list[1] , frames[i][1])
+    cmds.setKeyframe('blendShape7.'+list[0])
+    
+    numi=0
+    for j in frames[i]:
+        cmds.setAttr('blendShape7.'+list[numi] , j)
+        cmds.setKeyframe('blendShape7.'+list[numi])
+        numi=numi+1
+        
+                
+
+"""
+for blendshapes in frames:
+    print (blendshapes)
+
+
+for blendshapes in frames:
+    for idx in range(len(blendshapes)):
+        print (idx, blendshapes)
+        #get blendshape channel by blendshape name
+        #list[idx]
+        #set the value to grabbed bs channel
+        #base_V3 blendShape7 
+        #channel.setattr(blendshapes[idx])
+        #cmds.setAttr('blendShape7.'+list[])
+"""        
+
