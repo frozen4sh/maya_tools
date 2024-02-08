45314c7f51fddd2a4d9a828ab976b2b67758465d
frozen4sh <hyh1411@hotmail.com>
2024-02-08 10:18:27 +0900

    20240208

--- 2024_1Q/MayaUITemplate.py
+++ 2024_1Q/MayaUITemplate.py
@@ -29,18 +29,55 @@ class MayaUITemplate(QtWidgets.QWidget):
         # set initial window size
         self.resize(1000, 800)      
         # locate UI widgets
-        self.btn_add = self.widget.findChild(QtWidgets.QPushButton, 'btn_add')        
+        self.btn_addBone = self.widget.findChild(QtWidgets.QPushButton, 'btn_addBone')        
         self.btn_close = self.widget.findChild(QtWidgets.QPushButton, 'btn_close')        
+        self.scroll_list_bone = self.widget.findChild(QtWidgets.QListWidget,'scroll_list_L')
         # assign functionality to buttons
-        self.btn_add.clicked.connect(self.add)
+        self.btn_addBone.clicked.connect(self.add)
         self.btn_close.clicked.connect(self.closeWindow)
+        self.bone_list = []  # Replace with your bone names
+        
+        
+        # Add items to the list
+        self.items = ['Item 1', 'Item 2', 'Item 3']
+        
+
+
     
     """
     Your code goes here
     """
     def add(self):
-        cmds.polySphere()
-        print ('add pressed')
+        # cmds.polySphere()
+        # print ('Add Bone Button is pressed')
+        
+        # Get the selected bone
+        selected_bone = cmds.ls(selection=True, type="joint")
+        if not selected_bone:
+            cmds.warning("Please select a bone.")
+            return
+        
+        print("a")
+        self.scroll_list_bone.addItem(selected_bone[0])
+
+        # Add the selected bone to the bone_list
+        # self.scroll_list_bone.addItems(selected_bone[0])
+        
+
+        
+
+        # Update the bone text scroll list with the new data
+        # self.update_text_scroll_list(self.scroll_list_bone, self.bone_list)
+
+        print(f"Bone '{selected_bone[0]}' added to the list.")
+
+    def update_text_scroll_list(self, text_scroll_list, data_list):
+        # Update the text scroll list with the given data list
+        # cmds.textScrollList(text_scroll_list, edit=True, removeAll=True)
+        # cmds.textScrollList(text_scroll_list, edit=True, append=data_list)
+        pass
+
+  
 
         
     def resizeEvent(self, event):
