d64c23674efc10d60d65e000e2023602d7eaeb11
frozen4sh <hyh1411@hotmail.com>
2024-02-20 14:39:38 +0900

    20240220

--- 2024_1Q/MayaUITemplate.py
+++ 2024_1Q/MayaUITemplate.py
@@ -1,10 +1,16 @@
 """
+Justin's FK -> Controller Tool
+Created by Justin Yehun Hwang 20/02/2024
+This tool helps to connect between controller and exist bone animaiton.
+
 Maya/QT UI template
 Maya 2023
 """
 
 import maya.cmds as cmds
 import maya.mel as mel
+import maya.api.OpenMaya as om
+
 from maya import OpenMayaUI as omui
 from shiboken2 import wrapInstance
 from PySide2 import QtUiTools, QtCore, QtGui, QtWidgets
@@ -26,20 +32,27 @@ class MayaUITemplate(QtWidgets.QWidget):
         self.widgetPath = ('D:\\Justin\\Python\\VIVE_Python\\QT_Test\\demo.ui')
         self.widget = QtUiTools.QUiLoader().load(self.widgetPath)
         self.widget.setParent(self)
+        
         # set initial window size
         self.resize(1000, 800)      
+        
         # locate UI widgets
         self.btn_addBone = self.widget.findChild(QtWidgets.QPushButton, 'btn_addBone')        
         self.btn_addCtrl = self.widget.findChild(QtWidgets.QPushButton, 'btn_addCtrl')        
+        self.btn_connect = self.widget.findChild(QtWidgets.QPushButton, 'btn_connect')        
+        self.btn_clearList = self.widget.findChild(QtWidgets.QPushButton, 'btn_clearList')        
         self.btn_close = self.widget.findChild(QtWidgets.QPushButton, 'btn_close')        
         self.scroll_list_bone = self.widget.findChild(QtWidgets.QListWidget,'scroll_list_L')
         self.scroll_list_ctrl = self.widget.findChild(QtWidgets.QListWidget,'scroll_list_R')
+        
         # assign functionality to buttons
         self.btn_addBone.clicked.connect(self.addBone)
         self.btn_addCtrl.clicked.connect(self.addCtrl)
+        self.btn_connect.clicked.connect(self.connect)
+        self.btn_clearList.clicked.connect(self.clearList)
         self.btn_close.clicked.connect(self.closeWindow)
-        self.bone_list = []  # Replace with your bone names
-        
+        self.bone_list = []  # Replace with your bone names         
+      
         
         # Add items to the list
         self.items = ['Item 1', 'Item 2', 'Item 3']
@@ -51,53 +64,73 @@ class MayaUITemplate(QtWidgets.QWidget):
     Your code goes here
     """
     def addBone(self):
-        # cmds.polySphere()
-        # print ('Add Bone Button is pressed')
         
         # Get the selected bone
         selected_bone = cmds.ls(selection=True, type="joint")
         if not selected_bone:
             cmds.warning("Please select a bone.")
             return
-        
-        print("a")
+                
         self.scroll_list_bone.addItem(selected_bone[0])
 
-        # Add the selected bone to the bone_list
-        # self.scroll_list_bone.addItems(selected_bone[0])
-        
-
-        
-
-        # Update the bone text scroll list with the new data
-        # self.update_text_scroll_list(self.scroll_list_bone, self.bone_list)
 
         print(f"Bone '{selected_bone[0]}' added to the list.")
     
     def addCtrl(self):
-        # cmds.polySphere()
-        # print ('Add Bone Button is pressed')
-        
-        # Get the selected bone
-        selected_ctrl = cmds.ls(selection=True, type="transform")
-        if not selected_ctrl:
+                
+        # nurbsCurve Finder
+        somethingSelected = om.MGlobal.getActiveSelectionList()
+        obj = somethingSelected.getDependNode(0)
+        shape = om.MFnDagNode(obj).child(0)
+        mfnDepNode = om.MFnDependencyNode(shape)
+        typeName = mfnDepNode.typeName
+
+        selected_ctrl = cmds.ls(selection=True)
+
+        # Get the selected ctrl
+        if typeName == "nurbsCurve":
+            self.scroll_list_ctrl.addItem(selected_ctrl[0])            
+
+
+        if not typeName == "nurbsCurve":
             cmds.warning("Please select a controller.")
-            return
-        
-        print("a")
-        self.scroll_list_ctrl.addItem(selected_ctrl[0])
-
-        # Add the selected bone to the bone_list
-        # self.scroll_list_bone.addItems(selected_bone[0])
-        
-
-        
-
-        # Update the bone text scroll list with the new data
-        # self.update_text_scroll_list(self.scroll_list_bone, self.bone_list)
+            return          
+       
 
         print(f"Bone '{selected_ctrl[0]}' added to the list.")
-       
+    
+    def connect(self):
+        # Connect each pair of bone and controller using parent constraint
+        
+        
+        ctrl_item_count = self.scroll_list_ctrl.count()
+        ctrl_items = [self.scroll_list_ctrl.item(i).text() for i in range(ctrl_item_count)]
+        print("Items in QListWidget:", ctrl_items) 
+
+        bone_item_count = self.scroll_list_ctrl.count()
+        bone_items = [self.scroll_list_bone.item(i).text() for i in range(bone_item_count)]
+        print("Items in QListWidget:", bone_items) 
+
+
+        for bone, controller in zip(bone_items, ctrl_items):
+            if cmds.objExists(bone) and cmds.objExists(controller):
+                # Check if the pair exists and apply parent constraint
+                constraint = cmds.parentConstraint(bone, controller, maintainOffset=True)
+                print(f"Connected: {bone} -> {controller}")
+            else:
+                cmds.warning(f"Skipping connection for pair: {bone} -> {controller}. One or both objects do not exist.")
+
+        print("Connection completed.")
+        self.clearList()        
+
+
+
+    def clearList(self):
+        # clear both lists
+        self.scroll_list_bone.clear()
+        self.scroll_list_ctrl.clear()
+        print("List Cleared")    
+    
 
     def update_text_scroll_list(self, text_scroll_list, data_list):
         # Update the text scroll list with the given data list
@@ -106,8 +139,7 @@ class MayaUITemplate(QtWidgets.QWidget):
         pass
 
   
-
-        
+       
     def resizeEvent(self, event):
         """
         Called on automatically generated resize event
@@ -137,7 +169,7 @@ def openWindow():
     mayaMainWindow = wrapInstance(int(mayaMainWindowPtr), QtWidgets.QWidget)
     MayaUITemplate.window = MayaUITemplate(parent = mayaMainWindow)
     MayaUITemplate.window.setObjectName('myToolWindowName') # code above uses this to ID any existing windows
-    MayaUITemplate.window.setWindowTitle('Maya UI Template')
+    MayaUITemplate.window.setWindowTitle('Justin\'s FK -> Controller Tool')
     MayaUITemplate.window.show()
     
 openWindow()
